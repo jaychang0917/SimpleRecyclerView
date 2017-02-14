d51a64b4693cb21d1a9fc4ae854c2ca137d9b238
jay <jay.chang@redso.com.hk>
2017-02-15 00:20:59 +0800

    Update section header measure method

--- library/src/main/java/com/jaychang/srv/decoration/SectionHeaderItemDecoration.java
+++ library/src/main/java/com/jaychang/srv/decoration/SectionHeaderItemDecoration.java
@@ -159,8 +159,8 @@ public class SectionHeaderItemDecoration extends RecyclerView.ItemDecoration {
 
   private View getAndMeasureSectionHeader(RecyclerView parent, int position) {
     View sectionHeader = provider.getSectionHeaderView(getItem(position), position);
-    int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getWidth(), View.MeasureSpec.EXACTLY);
-    int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getHeight(), View.MeasureSpec.AT_MOST);
+    int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getWidth(), View.MeasureSpec.UNSPECIFIED);
+    int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getHeight(), View.MeasureSpec.UNSPECIFIED);
     sectionHeader.measure(widthSpec, heightSpec);
     return sectionHeader;
   }

7a6474f2d7f9cce5659e9666159943c2342f3fa4
jay <jay.chang@redso.com.hk>
2017-08-09 12:14:30 +0800

    Clean code

--- library/src/main/java/com/jaychang/srv/SimpleCell.java
+++ library/src/main/java/com/jaychang/srv/SimpleCell.java
@@ -3,6 +3,7 @@ package com.jaychang.srv;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 
